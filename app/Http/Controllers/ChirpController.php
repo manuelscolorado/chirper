d761b2cef205c37e8d79010129c5753e64572ac2
manuelscolorado <60707189+manuelscolorado@users.noreply.github.com>
2022-10-19 01:24:36 -0500

    Retrieving the Chirps

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -14,7 +14,6 @@ class ChirpController extends Controller
      */
     public function index()
     {
-
         return view('chirps.index', [
 
             'chirps' => Chirp::with('user')->latest()->get(),
